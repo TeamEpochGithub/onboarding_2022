d3d193805d2d393b3ea85d81a2d6418d24c96637
sietse4 <sietse@storytemple.com>
2022-05-27 13:25:00 +0200

    updates

--- competition/evaluate_public.py
+++ competition/evaluate_public.py
@@ -1,4 +1,7 @@
-def score_submission(path_to_csv: str, mode='mse') -> float:
+import pandas as pd
+
+
+def score_submission_path(path_to_csv: str, mode='mse') -> float:
     """
     Public method to score submission. The actual script that does the calculations is hidden to not reveal any answers.
     Use this method to score your submissions
@@ -6,9 +9,14 @@ def score_submission(path_to_csv: str, mode='mse') -> float:
     Your submission csv should at least contain two columns, one named carId and one named price.
     It should contain all carId's in the ford_test.csv file, combined with your price prediction
 
-    :param path_to_df: Path to your submission csv file
+    :param path_to_csv: Path to your submission csv file
     :param mode: Scoring metric. Options are 'mse' and 'mae' for mean squared or mean absolute error
     :return: The MSE or MAE value as float
     """
     import evaluate
     return evaluate.score_submission(path_to_csv, mode=mode)
+
+
+def score_submission_df(df: pd.DataFrame, mode='mse') -> float:
+    df.to_csv("../data/temp_submission.csv", index=False)
+    return score_submission_path("../data/temp_submission.csv", mode=mode)
--- competition/preprocess.py
+++ competition/preprocess.py
@@ -1,11 +1,42 @@
 import pandas as pd
+from evaluate_public import *
 import numpy as np
-from sklearn.model_selection import train_test_split
-import pickle
-import hashlib
-import py_compile
 
-# py_compile.compile("evaluate_todelete.py", "evaluate_todelete.pyc", None, True)
+df_train = pd.read_csv("../data/ford_train.csv")
+df_test = pd.read_csv("../data/ford_test.csv")
 
-from evaluate_public import score_submission
-print(score_submission("../data/ford_test.csv"))
+
+def normalize(df: pd.DataFrame) -> pd.DataFrame:
+    """Method to normalize an entire DataFrame"""
+    return df.apply(lambda l: l / max(l), axis=0)
+
+
+def convert_column_types(df: pd.DataFrame) -> pd.DataFrame:
+    """Function to convert variables to one common type (float)"""
+    # Preprocessing columns in one loop to floats
+    for column_name, column_dtype in zip(df.columns, df.dtypes):
+
+        # Categorical to int
+        if column_dtype == "object":
+            df[column_name] = pd.factorize(df[column_name])[0]
+
+        # All columns to float
+        df[column_name] = list(map(float, df[column_name]))
+
+    return df
+
+
+def make_predictions(predictor, X_test: np.array, df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Finish method to actually use the predictor to make predictions. Watch the following things:
+    - Make sure not to normalize the carId's
+    - If you normalize the price, this should be converted back before submitting
+    - Check all your columns, make sure it represents the information well
+
+    :param predictor: Some model with a predict function
+    :param df: Test dataframe
+    :return: Submission dataframe
+    """
+
+    submission = pd.DataFrame.from_dict({'carId': [], 'price': []})
+    return submission
